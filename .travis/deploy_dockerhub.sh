26684b3896c9d61148e5e2dbf271b263818b5903
Cryofracture <thecryoforce@gmail.com>
2021-02-21 05:33:31 -0600

    adjusted docker build and push command syntax and command-line result is what is expected.

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 set -x
+TRAVIS_REPO_SLUG="jira_travis"
+TRAVIS_BRANCH="master"
 docker login -u $DOCKER_USER -p $DOCKER_PASS
 if [ "$TRAVIS_BRANCH" = "master" ]; then
     TAG="latest"
 else
     TAG="$TRAVIS_BRANCH"
 fi
-docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG .
-docker push cryofracture/$TRAVIS_REPO_SLUG:$TAG
+docker build -f Dockerfile -t "cryofracture/$TRAVIS_REPO_SLUG:$TAG" .
+docker push "cryofracture/$TRAVIS_REPO_SLUG:$TAG"
